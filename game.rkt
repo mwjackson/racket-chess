787422bd4d805950ec9179fb529f6b6428bebdcd
Matt Jackson <email@mwjackson.net>
2014-09-27 21:46:46 +0100

    check the square contains the piece you're trying to move

--- game.rkt
+++ game.rkt
@@ -2,11 +2,14 @@
 (require "board.rkt")
 (require "moves.rkt")
 
-(define (state) 
-  (make-board))
-
 (define (is-square? sq algn)
-  (equal? (algn->coord algn) (square-coord sq))) 
+  (equal? (algn->coord algn) (square-coord sq)))
+
+(define (contains-piece? sq piece)
+  (eq? (square-piece sq) piece))
+
+(define (square-on-board algn board)
+  (findf (lambda (x) (is-square? x algn)) board))
 
 (define (remove-piece piece sq)
   (square (square-coord sq) null))
@@ -27,8 +30,10 @@
   (define from (second move))
   (define to (third move))
   (define moves (apply (move-map piece) (list (algn->coord from))))
+  (displayln moves)
   (cond 
     [(not (eq? 3 (length move))) #f]
+    [(not (contains-piece? (square-on-board from board) piece)) #f]
     [(not (member (algn->coord to) moves)) #f]
     [else #t]))
 
@@ -38,8 +43,8 @@
   (define command (read-line))
   (define move (string-split command))
   (cond 
-    [(string=? command "exit")  (displayln "exited successfully...")]        
+    [(string=? command "exit")  (displayln "goodbye!")]        
     [(valid-move? move board) (game-loop (move-piece (first move) (second move) (third move) board))]
-    [else (displayln "could not parse move") (game-loop board)]))
+    [else (displayln "not a valid move") (game-loop board)]))
 
-(define start (game-loop (make-board)))
+;(define start (game-loop (make-board)))
